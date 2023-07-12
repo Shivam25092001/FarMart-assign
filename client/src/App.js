ad9a25651bf49a66ab2d24444e793fb082d0aaa7
Shivam <ss056989@gmail.com>
2023-07-12 23:54:57 +0530

    deployment check file added

--- client/src/App.js
+++ client/src/App.js
@@ -5,11 +5,13 @@ import {
   Route
 } from "react-router-dom";
 import Home from './components/Home/home.js';
+import DeploymentCheck from './components/DeploymentCheck';
 
 function App() {
   return (
       <Router>
         <Routes>
+          <Route exact path="/check" element = {<DeploymentCheck/>} />
           <Route exact path="/get/:shortUrl" element={<Home />} />
           <Route exact path="/" element={<Home />} />
         </Routes>
