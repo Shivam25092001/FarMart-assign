ae893c5fac3f2afebae24f3fd24eaa7ecb960fb6
Shivam <ss056989@gmail.com>
2023-07-13 14:35:30 +0530

    README update

--- client/src/API_BASE_URL.js
+++
@@ -1,4 +0,0 @@
-// const baseUrl = "https://file-share-api-cmfs.onrender.com";
-
-const baseUrl = "http://localhost:5000";
-export default baseUrl;
--- client/src/components/DeploymentCheck.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const DeploymentCheck = () => {
-  return (
-    <div>DeploymentCheck</div>
-  )
-}
-
-export default DeploymentCheck
